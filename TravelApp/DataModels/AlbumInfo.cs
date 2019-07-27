4d734bec5e92d58335bb16e2ec407e11073b3384
AgTravellers <49646886+AgTravellers@users.noreply.github.com>
2019-07-27 20:46:42 +0530

    Add Albums page

---
+++ TravelApp/DataModels/AlbumInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelApp.DataModels
+{
+    public class AlbumInfo
+    {
+        public string AlbumName { get; set; }
+
+        public string ContainerName { get; set; }
+
+        public AlbumInfo(string albumname, string containername)
+        {
+            this.AlbumName = albumname;
+            this.ContainerName = containername;
+        }
+    }
+}
